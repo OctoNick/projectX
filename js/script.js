48c7114a78b1e04c6c994b2fe4bd38c9421359a9
Ivan <example@gmail.com>
2023-03-09 15:12:22 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+console.log('arr ' + "- object");
+console.log(4 + + "5");
+ 
+
+let incr = 10,
+    decr = 10;
+//++incr;
+//--decr;
+
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+//&& //используется как оператор "и"
+//|| //используется как оператор "или"
+
+const isChecked = false,
+      isClose = false;
+
+      console.log(isChecked || !isClose);
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}/5`);  - КОМАНДА ${ПЕРЕМЕННАЯ} ИСПОЛЬЗУЕТСЯ ДЛЯ ДИНАМИЧНОГО ПОДСТАВЛЕНИЯ ДАННЫХ 
+
+//const user = "Ivan";
+
+//alert(`Privet, ${user}`);
